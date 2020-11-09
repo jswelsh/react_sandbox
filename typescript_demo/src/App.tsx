0e15b37c881704b6fd95dbb6e38646acc99d7cb8
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-09 05:55:18 -0800

    added interval for reading time

--- typescript_demo/src/App.tsx
+++ typescript_demo/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { time } from 'console';
+import React, { useState } from 'react';
 import './App.css';
 
 interface IAppOwnProps {
@@ -6,10 +7,23 @@ interface IAppOwnProps {
   userType:'admin' | 'moderator' | 'user' | 'guest';
 }
 
-const App: React.FC = (): JSX.Element => {
+const App: React.FC<IAppOwnProps> = ({userType, username}): JSX.Element => {
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+  
+  setInterval(() => {
+    setInterval(() => {
+      setTime(new Date(Date.now()));
+    }, 1000)
+  })
+
   return (
     <div className="App">
-      fubar
+      <p>
+        hi, {username ? username : 'Mysterious User'}, your user type is {username? userType : 'irrelevant because I do not know you'}
+      </p>
+      <p>
+        {time.toUTCString()}
+      </p>
     </div>
   );
 }
