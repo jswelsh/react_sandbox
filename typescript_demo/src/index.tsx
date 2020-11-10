c96e86b09c73c9ebfba004ff45a3cc11467c775e
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-09 21:21:57 -0800

    setting up middleware for redux

--- typescript_demo/src/index.tsx
+++ typescript_demo/src/index.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from '/store';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -8,22 +10,24 @@ import { UserList } from './UserList';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<BrowserRouter>
-  <Switch>
-    <Route
-      exact
-      path='/'
-      render={(props) => <App userType='admin' username='h4x0r' {...props} />}
-    />
-  </Switch>
-  <Switch>
-    <Route
-      exact
-      path='/userlist'
-      render={(props) => <UserList {...props} />}
-    />
-  </Switch>
-</BrowserRouter>,
+<Provider store={store}>
+  <BrowserRouter>
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={(props) => <App userType='admin' username='h4x0r' {...props} />}
+      />
+    </Switch>
+    <Switch>
+      <Route
+        exact
+        path='/userlist'
+        render={(props) => <UserList {...props} />}
+      />
+    </Switch>
+  </BrowserRouter>
+</Provider>,
 document.getElementById('root')
 );
 
