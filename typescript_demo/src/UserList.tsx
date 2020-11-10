5547d031ec419713db165f1792496a4aa8d98b99
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-09 22:20:12 -0800

    .

--- typescript_demo/src/UserList.tsx
+++ typescript_demo/src/UserList.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Dispatch, AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux';
+import { IAppState } from './store/RootReducer';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { IUser } from './store/user/UserTypes';
+
+
 const CenterContent = styled.div`
 	text-align: center;
 	`;
 
 interface IUserListOwnProps {
 
+}
+
+interface IUserListStateToProps {
+  user: IUser
 }
 
 export const UserList: React.FC<IUserListOwnProps> = (): JSX.Element => {
