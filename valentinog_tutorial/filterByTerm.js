dd305acb367187fb48212e0ad2b41142c5e90153
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-17 22:15:09 -0800

    adding proper typing

---
+++ valentinog_tutorial/filterByTerm.js
@@ -0,0 +1,12 @@
+"use strict";
+function filterByTerm(input, searchTerm) {
+    if (!searchTerm)
+        throw Error("searchTerm cannot be empty");
+    if (!input.length)
+        throw Error("inputArr cannot be empty");
+    var regex = new RegExp(searchTerm, "i");
+    return input.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+filterByTerm(['string1', 'string2', 'string3'], 'java');
