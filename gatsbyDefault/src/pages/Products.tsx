641b32835f9aabad18fd3e445b9ab822a5345d25
jswelsh <james.sinclair.welsh@gmail.com>
2021-02-14 10:08:03 -0800

    playing around with gatsby image

--- gatsbyDefault/src/pages/Products.tsx
+++ gatsbyDefault/src/pages/Products.tsx
@@ -1,11 +1,12 @@
 // If you don't want to use TypeScript you can delete this file!
 import React, { useEffect, useRef, useState } from "react"
-import { PageProps, Link, graphql } from "gatsby"
+import { PageProps, Link, useStaticQuery, graphql } from "gatsby"
 import { makeStyles } from '@material-ui/core/styles';
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Box, Container, Divider, Typography } from "@material-ui/core"
+import { Box, Container, Divider, Paper, Typography } from "@material-ui/core"
+// import Image from "../components/image"
+import Img from "gatsby-image"
 
 const useStyles = makeStyles((theme) => ({
 }));
@@ -26,25 +27,48 @@ function useOnScreen(ref) {
 
   return isIntersecting
 }
+export const query = graphql`
+query {
+      placeholderImage: file(relativePath: { eq: "016.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1080) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `
 
-const Products/* : React.FC<PageProps<DataProps>>  */= (/* { data, path } */) => {
+const Products = () => {
   const ref = useRef()
   const classes = useStyles();
-
+  const data = useStaticQuery(query)
+/*   const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `) */
   const onScreen = useOnScreen(ref)
   const allowedProps = { ref }
   return (
   <Layout test={onScreen}>
     <SEO title="Products" />
-    <Box {...allowedProps } style={{background:"#00F08E", height:"400px", display:"flex", alignItems:'center'}}>
-      <Container maxWidth="sm" component="main" /* className={classes.heroContent} */>
-        <Typography variant='subtitle1'>Tech solutions</Typography>
+    <Box {...allowedProps } style={{background:"#00af69",paddingTop:"40px", height:"300px", display:"flex",flexDirection:'column'/* , justifyContent:'center' */, alignItems:'center'}}>
+      <Container maxWidth="lg" component="main">
+        <Typography variant='subtitle1'>Tech solutions  </Typography>
         <Divider/>
         <Typography variant='h1' align="center">LO·FI</Typography>
       </Container>
-
     </Box>
-    {/* {onScreen && "I'm on screen!"} */}
+      <Container component="main" maxWidth="lg" style={{transform: "translate(0px, -120px)"}} /* className={classes.heroContent} */>
+        <Img fluid={data?.placeholderImage?.childImageSharp?.fluid} />
+      </Container>
     <Typography variant='h1'>Products</Typography>
     <Typography variant='h1'>Products</Typography>
     <Typography
