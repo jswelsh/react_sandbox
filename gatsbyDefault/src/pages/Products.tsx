9002abcabfdd2559327aac170fe34e2c00b347dd
jswelsh <james.sinclair.welsh@gmail.com>
2021-02-16 21:34:11 -0800

    .

--- gatsbyDefault/src/pages/Products.tsx
+++ gatsbyDefault/src/pages/Products.tsx
@@ -4,7 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Box, Card, CardContent, Container, Divider, Grid, Grow, List, ListItem, ListItemText, Slide, Typography } from "@material-ui/core"
+import { Box, Card, CardContent, CardMedia, Container, Divider, Grid, Grow, List, ListItem, ListItemText, Slide, Typography } from "@material-ui/core"
 import Img from "gatsby-image"
 import clsx from  'clsx'
 
@@ -37,34 +37,41 @@ const useStyles = makeStyles((theme) => ({
     alignSelf: 'flex-start'
   },
   productSection: {
-    paddingTop: theme.spacing(4),
+    paddingTop: theme.spacing(10),
     transform: "translate(0px, -100px)",
     [theme.breakpoints.up('sm')]: {
       transform: "translate(0px, -160px)",
-      paddingTop: theme.spacing(6),
+      paddingTop: theme.spacing(16),
     },
     [theme.breakpoints.up('md')]: {
       transform: "translate(0px, -290px)",
-      paddingTop: theme.spacing(8),
+      paddingTop: theme.spacing(20),
+    }
+  },
+  BrandPillar:{
+    margin: ' 0px 32px 64px 32px',
+    [theme.breakpoints.up('sm')]: {
+      margin: '0px 64px 96px 64px'
+    },
+    [theme.breakpoints.up('md')]: {
+      margin: '0px 64px 128px 64px'
     }
   },
   productsList: {
     overflow: 'hidden',
     display:'flex',
-    alignItems:'center',  
+    justifyContent:'center',  
     padding:theme.spacing(2),
-    // flexDirection:'column',
-    // maxWidth: 1000
   },
   productsTitle: {
-    padding:'16px',
-    display: 'inline-block',
+    padding:'32px 32px 8px 32px',
+    // fontSize: '4rem',
     [theme.breakpoints.up('sm')]: {
-      paddingBottom: theme.spacing(6),
+      // paddingBottom: theme.spacing(6),
     },
     [theme.breakpoints.up('md')]: {
-      fontSize: '6rem',
-      paddingBottom: theme.spacing(8),
+      // fontSize: '6rem',
+      // paddingBottom: theme.spacing(8),
     },
   },
   underline: {
@@ -79,7 +86,6 @@ const useStyles = makeStyles((theme) => ({
       height: '32px',
       transform: 'translate(16px, 100px)',
     }
-    // borderBottom:'solid 15px #00af69',
   },
   Primary: {
     color:'#00af69'
@@ -87,30 +93,12 @@ const useStyles = makeStyles((theme) => ({
   Divider: {
     background: '#00af69'
   },
-/*   productItem: {
-    padding: theme.spacing(2),
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '520px'
-    }
-  }, */
-  fromLeft: {
-/*     [theme.breakpoints.up('md')]: {
-      flexDirection:'row',
-    } */
-  },
-  fromRight: {
-/*     [theme.breakpoints.up('md')]: {
-      flexDirection:'row-reverse',
-    } */
-  },
   productTitle: {
     '&:first-letter': {
       fontSize: '200%',
       color: '#00af69'
     },
   },
-  productDescription: {
-  }
 }));
 
 function useOnScreen(ref) {
@@ -147,7 +135,7 @@ const query = graphql`
   }
 `
 
-const ProductSubTitle = ({emphasis = false, direction, primaryText, secondaryText = null, domRef}) => {
+const ProductSubTitle = ({emphasis = false, direction, primaryText = null, secondaryText = null, domRef}) => {
   const [isVisible, setVisible] = useState(false);
   const [isVisibleDelay, setVisibleDelay] = useState(false);
 
@@ -176,7 +164,7 @@ const ProductSubTitle = ({emphasis = false, direction, primaryText, secondaryTex
   <Grid
     container
     direction="row">
-    <Slide
+    {primaryText && <Slide
       direction={direction}
       in={isVisible}
       mountOnEnter
@@ -189,7 +177,7 @@ const ProductSubTitle = ({emphasis = false, direction, primaryText, secondaryTex
           children={primaryText}
         />
       </div>
-    </Slide>
+    </Slide>}
     <Grow
       in={isVisibleDelay}
       mountOnEnter
@@ -199,7 +187,7 @@ const ProductSubTitle = ({emphasis = false, direction, primaryText, secondaryTex
         {secondaryText && <Typography
           className={emphasis ? classes.Primary : null }
           variant='h3'
-          style={{textTransform: 'uppercase', display:'inline', paddingLeft:'16px'}}
+          style={{textTransform: 'uppercase', display:'inline', paddingLeft: primaryText ? '16px' : '0px'}}
           children={secondaryText}
         />}
       </div>
@@ -219,7 +207,7 @@ const ProductItem = ({
 
   const [isVisible, setVisible] = useState(false);
   const options = {
-    rootMargin: '-200px 0px -200px 0px',
+    rootMargin: '-100px 0px -100px 0px',
     threshold: 1
   }
   const domRef = useRef();
@@ -237,75 +225,37 @@ const ProductItem = ({
     return () => observer.unobserve(domRef.current)// clean up
   }, []);
   return (
-  <Grid
-    ref={domRef}
-    item
-    md={6}
-    lg={6}
-    xl={6}
-    spacing={3}
-    style={{minHeight:'500px', maxWidth:'700px',padding:'32px 64px', justifyContent:'center'}}>
-    <Slide direction={direction} in={isVisible} mountOnEnter timeout={800} >
-      <Card className={
-        direction === 'right'
-          ? classes.fromRight
-          : classes.fromLeft}>
-        {image}
-        <CardContent>
-          <Typography
-            variant='h3'
-            className={classes.productTitle}
-            children={title}/>
-          <Typography
-            component="span"
-            variant="body2"
-            color="textPrimary"
-            children={description}/>
-        </CardContent>
-      </Card>
-{/*     </ListItem> */}
-  </Slide>
-  </Grid>
-
+    <Grid
+      ref={domRef}
+      item
+      xs={12}
+      sm={12}
+      md={6}
+      lg={6}
+      xl={6}
+      spacing={3}
+      style={{minHeight:'200px', maxWidth: '600px', padding:'32px 64px', justifyContent:'center'}}>
+      <Slide direction={direction} in={isVisible} mountOnEnter timeout={1200} >
+        <Card style={{maxWidth: '500px'}}>
+          <CardMedia>
+            {image}
+          </CardMedia>
+          <CardContent>
+            <Typography
+              variant='h3'
+              className={classes.productTitle}
+              children={title}/>
+            <Typography
+              component="span"
+              variant="body2"
+              color="textPrimary"
+              children={description}/>
+          </CardContent>
+        </Card>
+      </Slide>
+    </Grid>
   );
 };
-
-/* 
-
-  <Grid item md={6}>
-  <div ref={domRef} style={{minHeight:'500px'}}>
-    <Slide direction={direction} in={isVisible} mountOnEnter timeout={800} >
-    <ListItem 
-      className={clsx(direction === 'right' ? classes.fromRight : classes.fromLeft, classes.ListItem)}
-      key='456789'>
-      <Container
-        maxWidth="sm"
-        children={image}
-        />
-      <ListItemText
-        primary={
-          <Typography
-            variant='h3'
-            className={classes.productTitle}
-            children={title}/>
-        }
-        className={classes.productItem}
-        secondary={
-          <Typography
-            component="span"
-            variant="body2"
-            color="textPrimary"
-          >
-            {description}
-          </Typography>
-        }
-      />
-    </ListItem>
-  </Slide>
-  </div>
-  </Grid>
-*/
-
 
 const Products = () => {
   const classes = useStyles();
@@ -313,6 +263,7 @@ const Products = () => {
 
   const ref = useRef()
   const refTwo = useRef()
+  const refThree = useRef()
 
   const onScreen = useOnScreen(ref)
 
@@ -320,7 +271,7 @@ const Products = () => {
   return (
   <Layout test={onScreen}>
     <SEO title="Products" />
-    <Box 
+    <Box
       {...allowedProps }
       className={classes.heroContent}>
       <Container
@@ -341,13 +292,20 @@ const Products = () => {
       className={classes.heroImage}>
       <Img fluid={data?.placeholderImage?.childImageSharp?.fluid} />
     </Container>
-    <div className={classes.productSection}>
-      <Box style={{display:'flex', flexDirection: 'column'}}>
+    <Box className={classes.productSection}>
+    {/* <Box style={{
+        display:'flex', 
+        flexDirection: 'column',
+        background: 'rgb(0 0 0 / 20%)'
+        }}> */}
+      <Box
+      style={{display:'flex', flexDirection: 'column'}}
+      >
         <div ref={refTwo} style={{ padding: '0px 64px 32px 64px'}}>
           <ProductSubTitle
             emphasis={false}
             direction={'left'}
-            primaryText={'manufactured'}
+            primaryText={'forged'}
             domRef={refTwo}/>
           <Divider /* className={classes.Divider}  *//>
           <ProductSubTitle
@@ -359,9 +317,9 @@ const Products = () => {
           <Divider /* className={classes.Divider} */ />
         </div>
       </Box>
-      <div style={{margin: '0px 64px 250px 64px'}}>
+      <Box className={classes.BrandPillar}>
         <Typography
-          variant='h5'
+          variant='h6'
           style={{display:'inline'}}
           children={`
           The real world is a complex place, 
@@ -369,12 +327,12 @@ const Products = () => {
           `}
         />
         <Typography
-          variant='h5'
+          variant='h6'
           style={{display:'inline', borderBottom:'solid 5px #00af69'}}
           children={`overbuilt.`}
         />
         <Typography
-          variant='h5'
+          variant='h6'
           style={{display:'inline'}}
           children={`
           Pick up any of 
@@ -385,13 +343,18 @@ const Products = () => {
           regulation.
           `}
         />
-      </div>
+      </Box>
+      {/* </Box> */}
+      {/* <div className={classes.underline}/> */}
       <Typography
         variant="h3"
         className={classes.productsTitle}
-        children="Our Products"/>
+          children="Our Products"/>
+          <Divider style={{padding: '3px', margin: '0px 32px'}} className={classes.Divider} />
       <List className={classes.productsList}>
         <Grid 
+          style={{maxWidth:'1400px'}}
+          xs={12}
           container 
           spacing={3}
           justify="center"
@@ -459,7 +422,28 @@ const Products = () => {
           />
         </Grid>
       </List>
-    </div>
+    </Box>
+    <Box style={{
+        display:'flex', 
+        flexDirection: 'column',
+        background: 'rgb(0 0 0 / 20%)'
+        }}>
+      <div ref={refThree} style={{ padding: '0px 64px 32px 64px'}}>
+        <ProductSubTitle
+          emphasis={false}
+          direction={'left'}
+          primaryText={'cutting'}
+          domRef={refThree}/>
+        <ProductSubTitle
+          emphasis={true}
+          direction={'left'}
+          primaryText={'edge'}
+          secondaryText={'design'}
+          domRef={refThree}/>
+        <Divider /* className={classes.Divider}  *//>
+        <Divider /* className={classes.Divider} */ />
+      </div>
+    </Box>
   </Layout>
   )
 }
